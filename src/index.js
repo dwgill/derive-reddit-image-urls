aa395ee428531ff78994c839b81fdec9aa1612a1
Daniel Gill <dwgill@outlook.com>
2021-08-08 17:22:32 -0400

    cleanup the index file

--- src/index.js
+++ src/index.js
@@ -10,34 +10,72 @@ addEventListener('fetch', event => {
 
 /**
  * @param {Request} request
- * @returns {Promise<Response>}
+ * @returns {Promise<Request>}
  */
 async function handleRequest(request) {
-  if (request.method?.toLowerCase() === 'get') {
-    return new Response('hello world', {
-      headers: {
-        'content-type': 'text/plain;charset=UTF-8',
+  try {
+    if (request.method?.toUpperCase() === 'GET') {
+      return helloWorldResponse();
+    }
+    const json = await request.json();
+
+    if (
+      typeof json.url === 'string' &&
+      /https:.*reddit.*comments/i.test(json.url)
+    ) {
+      const result = await getImageDataFromRedditUrl(json.url);
+      return jsonResponse({
+        status: 'success',
+        result,
+      });
+    }
+
+    return jsonResponse(
+      {
+        status: 'failure',
+        result: 'invalid url',
       },
-    });
+      {
+        status: 400,
+      },
+    );
+  } catch (err) {
+    return errResponse(err);
   }
-  const json = await request.json();
-  let result = {};
-  if (
-    typeof json.url === 'string' &&
-    /https:.*reddit.*comments/i.test(json.url)
-  ) {
-    result = await getImageDataFromRedditUrl(json.url);
-  }
+}
 
-  const responseBody = {
-    status: 'success',
-    result,
-  };
-
-  const jsonResult = JSON.stringify(responseBody, null, 2);
-
-  return new Response(jsonResult, {
+function helloWorldResponse() {
+  return new Response('hello world', {
     headers: {
+      'content-type': 'text/plain;charset=UTF-8',
+    },
+  });
+}
+
+function errResponse(err) {
+  return jsonResponse(
+    {
+      status: 'error',
+      error: err,
+    },
+    {
+      status: 500,
+    },
+  );
+}
+
+/**
+ * @param {object} obj
+ * @param {ResponseInit?} responseInit
+ * @returns
+ */
+function jsonResponse(obj, { headers, status, statusText } = {}) {
+  const jsonStr = JSON.stringify(obj, null, 2);
+  return new Response(jsonStr, {
+    ...(status && { status }),
+    ...(statusText && { statusText }),
+    headers: {
+      ...headers,
       'content-type': 'application/json;charset=UTF-8',
     },
   });
